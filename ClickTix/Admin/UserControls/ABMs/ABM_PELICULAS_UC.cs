f8bc936b0b866b5c8f4ef6daabdc0b13b49e0cf8
TiagoWalterStansiola <tiago.stansiola@davinci.edu.ar>
2023-09-24 16:46:13 -0300

    prueba botones de tabla de peliculas

--- ClickTix/Admin/UserControls/ABMs/ABM_PELICULAS_UC.cs
+++ ClickTix/Admin/UserControls/ABMs/ABM_PELICULAS_UC.cs
@@ -42,8 +42,37 @@ namespace ClickTix.UserControls
 
         private void grid_peliculas_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+            // Verifica si se hizo clic en el botón "Modificar"
+            if (e.ColumnIndex == grid_peliculas.Columns["Modificar"].Index && e.RowIndex >= 0)
+            {
+                // Obtén el ID del registro seleccionado
+                int id = Convert.ToInt32(grid_peliculas.Rows[e.RowIndex].Cells["id"].Value);
 
+                // Abre un formulario o ventana para modificar el registro
+                // Puedes pasar el ID como parámetro al formulario de modificación
+                //FORM_PELICULAS_UC formModificarPelicula = new FORM_PELICULAS_UC(id);
+               // formModificarPelicula.ShowDialog();
+
+                // Después de modificar el registro, actualiza la vista si es necesario
+                // Puedes volver a cargar los datos en el DataGridView o actualizar la fila modificada
+                Form1_Load(grid_peliculas); // Llama a la función de carga nuevamente
+            }
+            // Verifica si se hizo clic en el botón "Borrar"
+            else if (e.ColumnIndex == grid_peliculas.Columns["Borrar"].Index && e.RowIndex >= 0)
+            {
+                // Obtén el ID del registro seleccionado
+                int id = Convert.ToInt32(grid_peliculas.Rows[e.RowIndex].Cells["id"].Value);
+
+                // Aquí debes implementar la lógica para eliminar el registro según el ID
+                // Puedes mostrar un mensaje de confirmación antes de la eliminación
+                DialogResult result = MessageBox.Show("¿Estás seguro de eliminar este registro?", "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    EliminarRegistro(id); // Reemplaza con tu propia función de eliminación
+                    Form1_Load(grid_peliculas); // Actualiza la vista después de la eliminación
+                }
+            }
         }
 
         private void Form1_Load(DataGridView tabla)
@@ -68,6 +97,41 @@ namespace ClickTix.UserControls
                
                     tabla.DataSource = dt;
                 }
+            }
+        }
+
+
+        public bool EliminarRegistro(int id)
+        {
+            try
+            {
+                using (MySqlConnection mysqlConnection = c.ObtenerConexion())
+                {
+                    mysqlConnection.Open();
+                    string query = "DELETE FROM peliculas WHERE id = @id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, mysqlConnection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Registro eliminado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se encontró el registro a eliminar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar el registro: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
             }
         }
 
