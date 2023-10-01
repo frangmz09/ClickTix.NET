a15c6835593ac1e4517c4eee044d7a5d512963e5
JuampiLampu <juan.lampugnani@davinci.edu.ar>
2023-10-01 19:16:31 -0300

    PROGRESO

---
+++ ClickTix/Empleado/ELEGIR_FUNCION_UC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickTix.Empleado
+{
+    public partial class ELEGIR_FUNCION_UC : UserControl
+    {
+        public ELEGIR_FUNCION_UC()
+        {
+            InitializeComponent();
+        }
+    }
+}
