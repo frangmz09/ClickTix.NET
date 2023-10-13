de0a0ffc23fa946a1b81621d3cf76f708b2501fe
JuampiLampu <juan.lampugnani@davinci.edu.ar>
2023-10-13 17:18:13 -0300

    act

--- ClickTix/Empleado/UserControls/TICKET_UC.Designer.cs
+++ ClickTix/Empleado/UserControls/TICKET_UC.Designer.cs
@@ -28,10 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TICKET_UC));
             this.button1 = new System.Windows.Forms.Button();
-            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
-            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             this.nombre_pelicula_ticket = new System.Windows.Forms.Label();
             this.fila_ticket = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -59,20 +56,6 @@
             this.button1.Text = "Confirmar e Imprimir Ticket";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // printPreviewDialog1
-            // 
-            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
-            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
-            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
-            this.printPreviewDialog1.Enabled = true;
-            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
-            this.printPreviewDialog1.Name = "printPreviewDialog1";
-            this.printPreviewDialog1.Visible = false;
-            // 
-            // printDocument1
-            // 
-            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
             // 
             // nombre_pelicula_ticket
             // 
@@ -266,8 +249,6 @@
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
-        private System.Drawing.Printing.PrintDocument printDocument1;
         private System.Windows.Forms.Label nombre_pelicula_ticket;
         private System.Windows.Forms.Label fila_ticket;
         private System.Windows.Forms.Label label1;
